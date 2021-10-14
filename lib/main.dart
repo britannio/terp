0586a145e0edbf2539115ef637032be0024f275c
Britannio <britanniojarrett@gmail.com>
2021-10-14 12:18:22 +0100

    lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:terp/terp_app.dart';
 import 'package:terp/terp_notifier.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Lock the device orientation to portrait
+  await SystemChrome.setPreferredOrientations(
+    <DeviceOrientation>[DeviceOrientation.portraitUp],
+  );
   await TerpNotifier.instance.init();
   runApp(const TerpApp());
 }
